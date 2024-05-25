6b5bd27fdb20e3b57b443d1964812239ac47bd10
Anahi Saucedo <ashleysaucedo18@gmail.com>
2024-05-24 19:07:44 -0600

    Imprimir (consola)  eventos en react: onClick, onChange

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,7 +1,15 @@
 import './CreateTodoButton.css'
 function CreateTodoButton(){
     return(
-     <button className="CreateTodoButton">+</button>
+     <button className="CreateTodoButton"
+      onClick={
+        (event) =>{
+         console.log ('Le diste click') //Todo lo que comience con on en react va a ser un evento transfroandolo en un AddEventListener. 
+         console.log(event)
+         console.log(event.target)
+        }
+        }
+     >+</button>
     );
   }
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,13 @@ import './TodoSearch.css'
 function TodoSearch() {
   return (
     <div className="TodoSearch">
-      <input id="todo-search-input" type="search" placeholder="Buscar tarea..." />
+      <input id="todo-search-input" type="search" placeholder="Buscar tarea..." 
+      onChange={(event) =>{
+        console.log(event); 
+        console.log('Escribiste en el TodoSearch');
+        console.log(event.target);
+        console.log(event.target.value);  //El input en este caso es target
+      }} />
     </div>
   );
 }
